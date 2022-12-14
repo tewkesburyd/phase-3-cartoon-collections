35785e928526515e51ad98dae8ab9f6955d8e65d
David Tewkesbury <tewkesburyd@hotmail.com>
2022-12-14 10:01:27 -0700

    commit tree dcaa11273956aff5ff84415929eb442d829225cd

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each.with_index(1) do |dwarve, index| puts "#{index}. #{dwarve}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet array
+  array.map do |item| "#{item.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls array
+  array.any? do |word| word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese array
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect do |cheese| cheese_types.include?(cheese)
+  end
 end
